a7cf90f0695cf11a2e95dde3fd278e7845bb0237
Guilherme Carrer <guilhermecarrerofc@gmail.com>
2024-06-02 19:55:10 -0300

    maisaprendizadoday2

---
+++ day2/aprendizado.py
@@ -0,0 +1,5 @@
+numeros = input("Digite dois número: ")
+novonum = int(numeros[0])
+novosnum2 = int(numeros[1])
+soma = novonum + novosnum2
+print(soma)
---
+++ day2/imc.py
@@ -0,0 +1,6 @@
+height = float(input("Qual a sua altura: "))
+weight = float(input("Qual é o seu peso: "))
+quadradoheight = (height**height)
+imc = (weight/quadradoheight)
+
+print(imc)
