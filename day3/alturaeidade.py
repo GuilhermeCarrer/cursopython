df1d576a4bce2f0d22e895ba008fad3c833c4e78
GuilhermeCarrer <guilhermercarrer@gmail.com>
2024-08-12 23:31:09 -0300

    avancos

--- day3/alturaeidade.py
+++ day3/alturaeidade.py
@@ -4,9 +4,21 @@ altura = int(input("Digite a sua altura em centimetros: "))
 if altura > 120:
     print("Você pode comprar o ingresso para a montanha russa!")
     idade = int(input("Digite a sua idade: "))
-    if idade <= 18:
-        print("Por favor pague R$7")    
+    if idade <= 12:
+        grana = 5
+        print("Por favor pague R$5")    
+    elif idade <=18:
+        grana = 7
+        print("Por favor pague R$7")
     else:
+        grana = 12
         print("Por favor pague R$12")
+
+    fotos = input("Você deseja tirar uma foto? SIM/NAO")
+    if fotos == "SIM":
+        print("Adicionado mais 3 R$")
+        grana = grana +3
+    else:
+        print("Ok, sem problemas")
 else:
     print("Você não possui altura suficiente para poder andar de montanha russa!")
